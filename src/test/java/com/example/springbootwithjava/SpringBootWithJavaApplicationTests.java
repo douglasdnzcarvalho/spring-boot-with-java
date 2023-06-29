1a67f7bd04c3c036e77a79c0303b6024a9e88152
Douglas Diniz Carvalho <douglasdnzcarvalho@gmail.com>
2023-06-29 12:41:46 -0300

    Projeto gerado através do Spring Initializr

---
+++ src/test/java/com/example/springbootwithjava/SpringBootWithJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootwithjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWithJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
