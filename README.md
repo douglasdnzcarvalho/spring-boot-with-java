6d92264006b568281017bc1dc32fb08edce5678b
Douglas Diniz Carvalho <douglasdnzcarvalho@gmail.com>
2023-07-02 20:20:55 -0300

    Adição do Flyway para gerenciamento das migrations

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # spring-boot-with-java
+
+Comando para executar as migrations através do plugin do maven sem ter que subir a aplicação:
+mvn flyway:migrate
---
+++ src/main/resources/db/migration/V2__populate_table_people.sql
@@ -0,0 +1,3 @@
+INSERT INTO `people` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+	(1, 'Patrocínio - Minas Gerais - Brasil', 'Douglas', 'male', 'Carvalho'),
+	(2, 'Uberlândia - Minas Gerais - Brasil', 'Victoria', 'female', 'Ferreira');
